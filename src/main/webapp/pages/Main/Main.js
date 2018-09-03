d7dba3182cb08c63e924658f01d1eba64c77232c
chandana weerakoon <chandana@medicalwizard.com.au>
2018-09-03 08:00:18 +0000

    Files modified - [Main,webapp] Files modified - [Main,webapp]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,8 +1,8 @@
-Application.$controller("MainPageController", ["$scope", function ($scope) {
+Application.$controller("MainPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
@@ -12,6 +12,13 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
          */
+    };
+
+
+    $scope.button1Tap = function($event, $isolateScope) {
+        if ($scope.Widgets.text1.datavalue && $scope.Widgets.text2.datavalue) {
+            $scope.Widgets.text3.datavalue = parseFloat($scope.Widgets.text1.datavalue) + parseFloat($scope.Widgets.text2.datavalue);
+        }
     };
 
 }]);
