f089a088f0a844965715f51b0ca06f7bfc39bba8
chandana weerakoon <chandana@medicalwizard.com.au>
2018-09-12 08:45:34 +0000

    cw New files added - [Nvd3_Chart] Files modified - [Main]

---
+++ src/main/webapp/WEB-INF/prefabs/Nvd3_Chart/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Nvd3_Chart",
+  "homePage" : "Main",
+  "name" : "Nvd3_Chart",
+  "platformType" : "DEFAULT",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.02"
+};
